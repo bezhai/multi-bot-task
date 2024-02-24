f23327220419cc202aeb0b97b85a12d1d3b86a95
yuanzhihong.chiwei <yuanzhihong.chiwei@bytedance.com>
2024-02-25 01:21:01 +0800

    fix

--- code/idl/server.thrift
+++ code/idl/server.thrift
@@ -11,11 +11,11 @@ service ChiweiBotService {
     conf.GetMemberValueResponse GetMemberValue(1: conf.GetMemberValueRequest request) (api.get="/api/need-auth/conf/get-member-value")
     conf.SetMemberValueResponse SetMemberValue(1: conf.SetMemberValueRequest request) (api.post="/api/need-auth/conf/set-member-value")
 
-    image_store.ListPixivImageMetaInfoResponse ListPixivImageMetaInfo(1: image_store.ListPixivImageMetaInfoRequest request) (api.get="/api/need-auth/image-store/list-info")
+    image_store.ListPixivImageMetaInfoResponse ListPixivImageMetaInfo(1: image_store.ListPixivImageMetaInfoRequest request) (api.post="/api/need-auth/image-store/list-info")
     image_store.UpdatePixivImageStatusResponse UpdatePixivImageStatus(1: image_store.UpdatePixivImageStatusRequest request) (api.post="/api/need-auth/image-store/update-status")
 
     translation.DeleteTranslationResponse DeleteTranslation(1: translation.DeleteTranslationRequest request) (api.post="/api/need-auth/translation/delete")
-    translation.ListTranslationResponse ListTranslation(1: translation.ListTranslationRequest request) (api.post="/api/need-auth/translation/list")
+    translation.ListTranslationResponse ListTranslation(1: translation.ListTranslationRequest request) (api.get="/api/need-auth/translation/list")
     translation.UpdateTranslationResponse UpdateTranslation(1: translation.UpdateTranslationRequest request) (api.post="/api/need-auth/translation/update")
 
     data_trans.ProxyResponse Proxy(1: data_trans.ProxyRequest request) (api.post="/api/need-sk/data-trans/proxy") // 代理请求
